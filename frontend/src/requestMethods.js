977d03116d073e59b92036b134ccbed120afab84
harshtakalkar <harshtakalkar037@gmail.com>
2025-08-30 14:54:28 +0530

    Update requestMethods.js

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const LocalBASE_URL="http://localhost:8000/api/";
 const BASE_URL = "https://pict-notes.onrender.com/api/";  
 
-export const pf = "https://pict-notes.onrender.com/images";  
+export const pf = "https://pict-notes.onrender.com";  
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
