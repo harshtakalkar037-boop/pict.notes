ab516c125a4fa679008d7910efb09a3e17de7438
harshtakalkar <harshtakalkar037@gmail.com>
2025-08-30 15:15:17 +0530

    Update SearchedUser.jsx

--- frontend/src/component/SearchedUser/SearchedUser.jsx
+++ frontend/src/component/SearchedUser/SearchedUser.jsx
@@ -50,7 +50,7 @@ const useStyles = makeStyles(theme => ({
 
 const SearchedUser = ({receiverUser}) => {
     // console.log(receiverUser)
-    const pf = "https://notesharingbackend-ankitkr437.onrender.com/images/";
+    const pf = "https://pict-notes.onrender.com/";
     const { currentUser:user} = useSelector((state) => state.user);
     const classes =useStyles();
     const [isbuilding ,setisbuilding] =useState(false)
