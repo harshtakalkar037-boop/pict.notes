43de85d6de1da40f7d4383b76e7dd3ac40f953bc
harshtakalkar <harshtakalkar037@gmail.com>
2025-08-30 14:30:57 +0530

    Update Topbar.jsx

--- frontend/src/component/topbar/Topbar.jsx
+++ frontend/src/component/topbar/Topbar.jsx
@@ -15,7 +15,7 @@ const Topbar = () => {
   const [searchedItem,setsearch] =useState("");
   const menu=useRef();
   const [placeholder, setplaceholder] = useState("..");
-  const pf="https://notesharingbackend-ankitkr437.onrender.com/images/";
+  const pf="https://pict-notes.onrender.com/images/";
   const dispatch=useDispatch();
   const MenuClickHandler=()=>{
     if(menu.current.style.display=="flex" )
