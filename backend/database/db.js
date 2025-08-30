e554a0777cd3280004bc24ee9925d69cca02142d
harshtakalkar <harshtakalkar037@gmail.com>
2025-08-30 12:45:05 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connection= async (URL)=>{
     try {
-        // const URL='mongodb+srv://harshtakalkar037_db_user:123@cluster0.ggowgdx.mongodb.net/pictnotes?retryWrites=true&w=majority';
+        // const URL='mongodb+srv://harshtakalkar037_db_user:123@cluster0.ggowgdx.mongodb.net/pictnotes?retryWrites=true&w=majority&appName=Cluster0';
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true});
         console.log('Database Connected Succesfully');
     } catch(error) {
