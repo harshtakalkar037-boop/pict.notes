276e26e8c73a2a121e6a98897343978256beb665
harshtakalkar <harshtakalkar037@gmail.com>
2025-10-14 21:47:57 +0530

    Update Homeprofile.jsx

--- frontend/src/pages/home/left/Homeprofile.jsx
+++ frontend/src/pages/home/left/Homeprofile.jsx
@@ -1,17 +1,15 @@
-
-
 import React from "react";
-import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux';
-import { Chat, AccountCircle, ExitToApp, Settings, Home } from "@material-ui/icons";
-import Media from '../../../loader/Loader.js';
 import "./HomeProfile.css";
-import '../../../component/topbar/Topbar.css';
+import { Link } from "react-router-dom";
+import Media from '../../../loader/Loader.js'
+import '../../../component/topbar/Topbar.css'
+import {useSelector} from 'react-redux';
+import { Chat,AccountCircle, ExitToApp,Settings,Home} from "@material-ui/icons";
 
 const Homeprofile = () => {
     const pf = "https://pict-notes.onrender.com/";
     
-    const { currentUser, isFetching } = useSelector((state) => state.user);
+    const {currentUser, isFetching} = useSelector((state) => state.user);
     const user = currentUser;
 
     const logouthandler = () => {
@@ -26,69 +24,45 @@ const Homeprofile = () => {
 
     return (
         <div className="homeprofile-container">
-            {/* User Profile Section */}
             <div className="homeprofile-header">
-                <Link 
-                    to={user ? `/profile/${user._id}` : `/`} 
-                    className="homeprofile-user-link"
-                >
-                    <img 
-                        src={(user && user.profilePicture) ? user.profilePicture : (pf + "DefaultPic.png")} 
-                        alt="Profile" 
-                        className="homeprofile-avatar" 
-                        onError={(e) => {
-                            e.target.src = pf + "DefaultPic.png";
-                        }}
-                    />
-                    <p className="homeprofile-username">
-                        {user?.username}
-                    </p>
+                <Link to={user ? `/profile/${user._id}` : `/`} className="homeprofile-user-link">
+                    <img src={(user && user.profilePicture) ? user.profilePicture : pf + "DefaultPic.png"} className="homeprofile-avatar" />
+                    <p className="homeprofile-username">{user?.username}</p>
                 </Link>
             </div>
             
-            {/* Navigation Menu */}
             <div className="homeprofile-menu">
                 <Link to={`/`} className="homeprofile-menu-item">
                     <div className="homeprofile-menu-content">
-                        <Home className="homeprofile-icon" />
+                        <Home style={{color: "#667eea"}}/>
                         <span className="homeprofile-menu-text">Home</span> 
                     </div>     
                 </Link>
                 
-                <Link 
-                    to={user ? `/profile/${user._id}` : `/`} 
-                    className="homeprofile-menu-item"
-                >
+                <Link to={user ? `/profile/${user._id}` : `/`} className="homeprofile-menu-item">
                     <div className="homeprofile-menu-content">
-                        <AccountCircle className="homeprofile-icon" />
+                        <AccountCircle style={{color: "#764ba2"}}/>
                         <span className="homeprofile-menu-text">View Profile</span> 
                     </div>     
                 </Link>
                 
-                <Link 
-                    to={`/messenger`} 
-                    className="homeprofile-menu-item"
-                >
+                <Link to={`/messenger`} className="homeprofile-menu-item">
                     <div className="homeprofile-menu-content">
-                        <Chat className="homeprofile-icon chat-icon" />
+                        <Chat style={{color: "#4299e1"}}/>
                         <span className="homeprofile-menu-text">Chat</span>
                     </div>
                 </Link>
                 
-                <Link 
-                    to={`/profile/update`} 
-                    className="homeprofile-menu-item"
-                >
+                <Link to={`/profile/update`} className="homeprofile-menu-item">
                     <div className="homeprofile-menu-content">
-                        <Settings className="homeprofile-icon" />
+                        <Settings style={{color: "#48bb78"}}/>
                         <span className="homeprofile-menu-text">Setting</span>
                     </div>
                 </Link>
                 
-                {/* Logout Button */}
                 <div className="homeprofile-menu-item logout-item" onClick={logouthandler}>
                     <div className="homeprofile-menu-content">
-                        <ExitToApp className="homeprofile-icon" />
+                        <ExitToApp style={{color: "#f56565"}}/>
                         <span className="homeprofile-menu-text">Logout</span>
                     </div>
                 </div> 
